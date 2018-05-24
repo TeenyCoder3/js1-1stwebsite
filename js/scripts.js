a0c514ad78b4844d17ac0cdb2890b95cb1f095be
Angelica Arellano <adainthe559@gmail.com>
2018-05-23 21:01:57 -0700

    day2

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var myName = "Angelica";
+
+console.log("I would like to learn all I can about Javascript and get all the knowledge I need to know about the Javascript. I hope this class will be challenging but fun :) Also my favorite hobby is Taekwondo, playing video games, or web developement.");
+
+// var myName = "Angelica";
+// let birthday = newDate ('3.31.2004');
+// const goal = 'To become a developer';
+// // console.log(name, birthday, goal);
+
+let favMovie = 'Jumanji';
+let favFood = 'steak';
+const movieYear = new Date(1/1/2017);
+const ingrediants = ['steak', 'rice', 'fries', 'fish'];
+
+alert(favFood + ingrediants);
+confirm(favMovie + movieYear);
