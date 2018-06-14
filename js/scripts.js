fcc4c7328fbe95165cc4185fcf7794327ec9adce
Angelica Arellano <adainthe559@gmail.com>
2018-06-14 03:58:34 -0700

    day7 finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,147 @@
-let body = document.getElementsByTagName('body'); //this will give you back an array.
-console.log(body);
+// DAY 6
+//
+// let body = document.getElementsByTagName('body'); //this will give you back an array.
+// console.log(body);
+//
+// let classes = document.getElementsByClassName("my-header");
+// console.log(classes[0]);
+//
+// let id = document.getElementById("my-paragraph");
+// console.log(id);
+//
+// //Newer Methods:
+// let tags = document.querySelector('body');
+// console.log(tag);
+//
+// let class = document.querySelector('.my-header');
+// console.log(myClass);
+//
+// let myId = document.querySelector('#my-paragraph');
+// console.log(myId);
+//
+// let tags = document.querySelectorAll('body');
+//
+// //Try it out day6
+// function queryDom(methodName, element) {
+//   switch(methodName.toLowerCase()) {
+//     case 'getelementsbytagname';
+//       document.getElementsByTagName(element);
+//       break;
+//     case 'querySelector':
+//       document.querySelector(`.${element}`);
+//       break;
+//     default;
+//       break;
+//   }
+// };
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// //         DAY 7
+//
+//
+//
+//
+//
+// //e stands for event
+//
+// let myClass = document.querySelector('my-header');
+//
+// myClass.addEventListener(
+//   'click', //first arg
+//   e => {  //second arg CALLBACK function
+//     alert('you clicked on me!!!');
+//   }
+// )
+//
+// console.log('hello world!');
 
-let classes = document.getElementsByClassName("my-header");
-console.log(classes[0]);
 
-let id = document.getElementById("my-paragraph");
-console.log(id);
 
-//Newer Methods:
-let tags = document.querySelector('body');
-console.log(tag);
+// let button = document.querySelector('.myBtn');
+// let div = document.querySelector('div');
+// let h1 = document.querySelector('h1');
+//
+// button.addEventListener('click', e => {
+//   e.target.textContent = "Goodbye Cats!";
+// })
+//
+// div.addEventListener('mouseenter', e => {
+//   e.target.style.backgroundColor = "#e40007";
+//   console.log(e);
+// })
+//
+// h1.addEventListener('mouseleave', e => {
+//   e.target.style.color = '#00ffff';
+// })
 
-let class = document.querySelector('.my-header');
-console.log(myClass);
 
-let myId = document.querySelector('#my-paragraph');
-console.log(myId);
 
-let tags = document.querySelectorAll('body');
 
-//Try it out day6
-function queryDom(methodName, element) {
-  switch(methodName.toLowerCase()) {
-    case 'getelementsbytagname';
-      document.getElementsByTagName(element);
-      break;
-    case 'querySelector':
-      document.querySelector(`.${element}`);
-      break;
-    default;
-      break;
+
+//  let button = document.querySelector('button');
+//  let div = document.querySelector('div');
+//  const body = document.querySelector('body');
+//  button.addEventListener('click', e => {
+//    e.target.textContent = "Goodbye Cats!";
+//  })
+//
+// div.innerHTML = "<p>lorem ipsum dolores cantu!!!</p>"; //This inserts the text in quotes into the div.
+// let newH1 = document.createElement('h1');
+// newH1.textContent = "Login Here!!";
+// //body.appendChild(newH1);
+// newH1.classList.add("text-center");
+// body.insertBefore(newH1, div);
+//
+// div.id = "red_text";
+//
+// div.classList.add('text-center');
+//
+// div.addEventListener('click', e => {
+//   div.classList.toggle('text-center'); //Comes back as true or false // Pretty cool stuff
+//   div.classList.replace('blue-background', 'green-background');
+// })
+// let myImg = document.createElement('img');
+//
+// //Set the src attribute
+// myImg.setAttribute('src', 'https://cbsnews2.cbsistatic.com/hub/i/r/2016/07/23/3dd92592-44a8-459b-8418-ffdc1cc05e60/resize/620x465/29246f341fc584bb4fb34db5bfd115df/enterprise-studio-model-dane-penland-smithsonian-national-air-and-space-museum.jpg');//Add the image to the page.
+//
+// div.appendChild(myImg);
+// console.log(myImg);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//COUNTING FROM 1 TO 100
+//IMPORTANT!!
+let body = document.querySelector('body');
+
+for (var i = 1, i <= 100, i++) {
+  let p = document.createElement('p');
+  if(i % 3 === 0 && i % 5 === 0) {
+    p.textContent = 'FizzBuzz';
+  } else if (i % 5 === 0) {
+    p.textContent = 'Buzz';
+  } else if (i % 3 === 0) {
+    p.textContent = 'Fizz';
+  } else {
+    p.textContent = i;
   }
-};
+  body.appendChild(p);
+}
